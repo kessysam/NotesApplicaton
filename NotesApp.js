9219bebc515380cbf823465d697087606ef5fcbd
kessysam <kessysam@gmail.com>
2016-08-17 11:10:53 +0100

    Note Application

--- NotesApp.js
+++ NotesApp.js
@@ -14,4 +14,21 @@
  		this.author = author;
  		this.notes = [];
  	}
+
+ 	/**
+ 	* This function takes the note content as the parameter
+ 	* and adds it to the notes list of the object.
+ 	* @param note_content
+ 	*/
+ 	create(note_content){
+ 		
+ 		this.author = note_content;
+ 		
+ 	}
+
+ 	listNotes(){
+
+ 		
+ 	}|
+ 
  }
